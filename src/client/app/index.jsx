7a649537f350b7fc396c0c3793db087fc5f707ec
Victor Nwaiwu <victor.nwaiwu@andela.com>
2017-05-14 18:28:53 +0100

    refactor code and group component accordingly

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,29 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import AwesomeComponent from './AwesomeComponent.jsx';
-import CardListComponent from './CardListComponent.jsx';
-import FormComponent from './FormComponent.jsx';
+import GithubIndexComponent from './GithubComponent/GithubIndexComponent.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state =  {
-      cards: []
-    }
-  };
-
-  addNewCard = (cardInfo) => {
-    this.setState(prevState => ({
-      cards: prevState.cards.concat(cardInfo)
-    }) )
   };
 
   render () {
     return (
-      <div>
+      <div className="container">
         <AwesomeComponent />
-        <FormComponent onSubmit={ this.addNewCard }/>
-        <CardListComponent cards={ this.state.cards }/>
+        <GithubIndexComponent />
       </div>
     );
   };
