b70aa32e9e764ea0a8ee27245f0da32f3f440545
Victor Nwaiwu <victor.nwaiwu@andela.com>
2017-05-14 15:40:17 +0100

    ft(card): create card component to display cards for each github user

---
+++ src/client/app/CardComponent.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const CardComponent = (props) => {
+  return (
+    <div style={{ margin: '1em'}}>
+      <img width="75" src={ props.avatar_url }/>
+      <div style={
+        { display: 'inline-block',
+          marginLeft: '10px'
+        }
+      }>
+        <div style={
+          {
+            fontSize: '1.25em',
+            fontWeight: 'bold',
+          }
+        }>{ props.name }</div>
+        <div>{ props.company }</div>
+      </div>
+    </div>
+  );
+}
+
+export default CardComponent;
